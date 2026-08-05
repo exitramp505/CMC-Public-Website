45fb19279858c495482236bd174a6acf641eaf7c
exitramp505 <119704679+exitramp505@users.noreply.github.com>
2026-08-05 15:37:59 -0400

    Update CMC Pathway links after site rename

--- lib/cmc-pathway.ts
+++ lib/cmc-pathway.ts
@@ -1,5 +1,5 @@
 export const CMC_PATHWAY_SIGNUP_URL =
-  "https://discernmentcenter.netlify.app/signup.html";
+  "https://cmc-pathway.netlify.app/signup.html";
 
 export const CMC_PATHWAY_LOGIN_URL =
-  "https://discernmentcenter.netlify.app/login.html";
+  "https://cmc-pathway.netlify.app/login.html";
